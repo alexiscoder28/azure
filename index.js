846b29d51bb4f4a621174740b87e21080b349e03
alexiscoder28 <146042312+alexiscoder28@users.noreply.github.com>
2025-12-04 02:23:34 -0500

    Push new code to staging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Staging');
+  res.send('Hello World from Staging Environment and its a test');
 });
 
 app.listen(port, () => {
