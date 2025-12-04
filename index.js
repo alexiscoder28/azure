2bf7a266507df7bee632da413a482ad3b0adf802
alexiscoder28 <146042312+alexiscoder28@users.noreply.github.com>
2025-12-04 01:59:39 -0500

    Update index.js for staging

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js 22 LTS!');
+  res.send('Hello World from Staging');
 });
 
 app.listen(port, () => {
